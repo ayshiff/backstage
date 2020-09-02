8bc3e032c33cd20b40bd946237169bcad82a201e
Rémi DOREAU <remi.d45@gmail.com>
2020-09-02 11:20:36 +0200

    refactor(core): chip component styling

--- packages/core/src/components/Chip/Chip.tsx
+++ packages/core/src/components/Chip/Chip.tsx
@@ -15,42 +15,13 @@
  */
 
 import React, { ComponentProps } from 'react';
-import { Chip as MaterialChip, makeStyles } from '@material-ui/core';
-
-import { BackstageTheme } from '@backstage/theme';
+import { Chip as MaterialChip } from '@material-ui/core';
 
 type Props = ComponentProps<typeof MaterialChip>;
-
-const isSmall = (size: Props['size']) => size === 'small';
-
-const useStyles = makeStyles<BackstageTheme, { size: Props['size'] }>(
-  theme => ({
-    chip: {
-      '& > span': {
-        color: theme.palette.grey[900],
-        lineHeight: `${theme.spacing(2.5)}px`,
-        fontWeight: theme.typography.fontWeightMedium,
-        fontSize: ({ size }) =>
-          `${theme.spacing(isSmall(size) ? 1.5 : 1.75)}px`,
-      },
-      '& > svg': {
-        width: ({ size }) => `${theme.spacing(isSmall(size) ? 2 : 3)}px`,
-        height: ({ size }) => `${theme.spacing(isSmall(size) ? 2 : 3)}px`,
-        color: theme.palette.grey[500],
-        margin: ({ size }) =>
-          `0 ${theme.spacing(isSmall(size) ? 0.5 : 0.75)}px 0 -${theme.spacing(
-            isSmall(size) ? 0.5 : 0.75,
-          )}px`,
-      },
-      backgroundColor: '#D9D9D9',
-    },
-  }),
-);
 
 /**
  * Thin wrapper on top of material-ui's Chip component
  */
-export const Chip = React.forwardRef<any, Props>((props, ref) => {
-  const classes = useStyles({ size: props.size });
-  return <MaterialChip className={classes.chip} ref={ref} {...props} />;
-});
+export const Chip = React.forwardRef<any, Props>((props, ref) => (
+  <MaterialChip ref={ref} {...props} />
+));
